f99e18e14ffae5138c91009261e0b39eec38e8d0
belemaaa <graceitamunoala@gmail.com>
2023-09-16 10:37:10 +0000

    auth

--- api/views.py
+++ api/views.py
@@ -6,6 +6,8 @@ from rest_framework.views import APIView
 from .authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.hashers import make_password, check_password
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 class Signup(APIView):
     authentication_classes = []
@@ -18,3 +20,33 @@ class Signup(APIView):
             username = serializer.validated_data.get('username')
             email = serializer.validated_data.get('email')
             raw_password = serializer.validated_data.get('password')
+            hashed_password = make_password(raw_password)
+            
+            user_exists = models.User.objects.filter(username=username)
+            if user_exists:
+                return Response({'error': 'user with this username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save(password = hashed_password)
+            return Response({'message': 'signup successful'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class Login(APIView):
+    authentication_classes = []
+    permission_classes = []
+    def post(self, request):
+        serializer = serializers.LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            username = serializer.validated_data.get('username')
+            password = serializer.validated_data.get('password')
+            try:
+                user = models.User.objects.get(username=username)
+            except models.User.DoesNotExist:
+                user=None
+            if user is not None and check_password(password, user.password):
+                token = Token.objects.get_or_create(user=user)
+                return Response({
+                    'message': 'login successful',
+                    'access_token': token.key
+                }, status=status.HTTP_200_OK)
+            return Response({'error': 'invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
